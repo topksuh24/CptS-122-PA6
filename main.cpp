d06b60a7043f4e468c0536408dfe10f8b64c9084
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-17 22:20:42 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+/* Programmer: Kenneth Suh
+ * Assignment: Programming Assignment 6 - Attendance Tracker
+ * Class: CptS 122 Lab Section 2
+ * Date: March 17, 2015
+ */
 
+#include "List.h"
+
+int main (void)
+{
+  
+  
+  return 0;
+}
