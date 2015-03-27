066fad1fa3f8b9c5106f556606fe983de28dd6dd
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-27 02:41:51 -0700

    Update List.h

--- List.h
+++ List.h
@@ -1,6 +1,16 @@
 #include "Node.h"
 
 // need to include anything else?
+// should also include everything that are from Node.h
+#include <iostream>
+#include <ifstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ifstream;
 
 // class
 class List
