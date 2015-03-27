54ddb66d4286658667861c83c3dca67e11e1fcd3
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-26 20:01:41 -0700

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -15,19 +15,31 @@ void menu()
 
 // Runs through the master linked list, displays each student’s name,
 // and prompts if he/she was absent for the current day.
-void markAbsent()
+void markAbsent() // need something in this parameter
 {
   
 }
 
 // Reads the .csv course file and overwrites the master linked list.
-void getCourseList()
+void getCourseList(Node list)
 {
-  
+  // need to include ifstream
+  ifstream file("file.csv");
+  string value = "";
+  // check if there is a file
+  if(file != NULL) // got the file
+  {
+    // overwrite the master linked list
+    
+  }
+  else // .csv file was not found
+  {
+    // you dont do anything
+  }
 }
 
 // Populates the master linked list with previous nodes from master.txt.
-void printMasterList()
+void printMasterList() // do i need anything in this parameter?
 {
   
 }
@@ -39,13 +51,13 @@ void printMasterList()
  * 2. Generate report for students with absences that match or exceed (the number entered by the user). 
  * 3. Generate date report.
 */
-void generateReport()
+void generateReport() // do i need anything in this parameter?
 {
   
 }
 
 // Stores the contents of the master linked list’s nodes to master.txt.
-void storeMasterList()
+void storeMasterList() // do i need anything in this parameter?
 {
   
 }
