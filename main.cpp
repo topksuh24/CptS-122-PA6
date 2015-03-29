180623cc3d73dbcc437c904a751f565cbc02faad
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-29 15:46:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* Programmer: Kenneth Suh, Jwahoon Kim, Sungsu Park
+/* Programmer: Kenneth Suh, Jwahoon Kim, Sungsoo Park
  * Assignment: Programming Assignment 6 - Attendance Tracker
  * Class: CptS 122 Lab Section 2
  * Date: March 17, 2015
@@ -8,7 +8,64 @@
 
 int main (void)
 {
-  
-  
-  return 0;
+	// declaration for csv
+	ifstream file("classList.csv");
+	string copyIndex;
+	file >> copyIndex;
+
+	//class declaration
+	List *courseList = new List();
+	Node *masterList = new Node();
+
+	int selection;
+
+	// use do while loop
+	do
+	{
+		// display the menu/choices
+		courseList -> menu();
+		cin >> selection;
+
+		switch(selection)
+		{
+			case 1:
+				// import course list
+				while(file.good())
+				{
+					Node *inputNode = new Node();
+					courseList -> getCourseList(masterList);
+				}
+				cout << "Data has been imported." << endl;
+				break;
+
+			case 2:
+				// load master list
+				while(masterList != NULL)
+				{
+					courseList -> printMasterList(masterList);
+				}
+				cout << "Master List is loaded." << endl;
+				break;
+
+			case 3:
+				// store master list
+				courseList -> storeMasterList(masterList);
+				cout << "Master List is stored." << endl;
+				break;
+
+			case 4:
+				// mark absences
+				courseList -> markAbsent(masterList);
+				cout << "You have marked absences." << endl;
+				break;
+
+			case 5:
+				// generate report
+				courseList -> generateReport(masterList);
+				cout << "Report is generated." << endl;
+				break;
+		}
+	} while(selection != 6);
+	
+	return 0;
 }
