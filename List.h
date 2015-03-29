0cf1cc37832fa9107f5d3946177abacf3b891b83
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-29 15:06:03 -0700

    Update List.h

--- List.h
+++ List.h
@@ -4,7 +4,6 @@
 
 #include "Node.h"
 
-// need to include anything else?
 // should also include everything that are from Node.h
 #include <iostream>
 #include <fstream>
@@ -13,34 +12,35 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::ios;
 using std::string;
 using std::ifstream;
 
 // class
 class List
 {
-  // friend the node class
-  friend class Node;
+	// friend the node class
+	//friend class Node;
   
-  // public
-  public:
-    // constructor
-    List(); // default
+	// public
+	public:
+		// constructor
+		List(); // default
     
-    // destructor
-    ~List();
+		// destructor
+		~List();
     
-    // other functions
-    void menu();
-    void markAbsent(Node *masterList);
-    void getCourseList(Node *report, ifstream &file);
-    void printMasterList(ifstream &master);
-    void generateReport(Node *report);
-    void storeMasterList();
+		// other functions
+		void menu();
+		void markAbsent(Node *masterList);
+		void getCourseList(Node *report);
+		void printMasterList(Node *report);
+		void generateReport(Node *report);
+		void storeMasterList(Node *report);
     
-  private:
-    Node *list;
-    Node *master; // do i need this?
+	private:
+		Node *list;
+		Node *master; // do i need this?
 };
 
 #endif
