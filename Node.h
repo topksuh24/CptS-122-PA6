27bdfbccbbf1b8fc1aef4984e41e8f8968f7e8ff
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-29 15:06:44 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -4,57 +4,67 @@
 
 // includes
 #include <iostream>
-#include <ifstream>
+#include <fstream>
 #include <string>
 
 //usings
 using std::cin;
 using std::cout;
 using std::endl;
+using std::ios;
 using std::string;
 using std::ifstream;
-
 using std::string;
+
+class List; // forward declaration
 
 // class
 class Node
 {
-  // public
-  public:
-    // constructor
-    Node(); // default for now
+	friend class List;
+
+	// public
+	public:
+		// constructor
+		Node(); // default for now
     
-    // destructor
-    ~Node();
+		// destructor
+		~Node();
     
-    // getters and setters
-    int getRecord();
-    int getID();
-    string getFirst();
-    string getLast();
-    string getEmail();
-    string getUnits();
-    string getMajor();
-    string getLevel();
+		// getters and setters
+		int getRecord();
+		int getID();
+		string getFirst();
+		string getLast();
+		string getEmail();
+		string getUnits();
+		string getMajor();
+		string getLevel();
+		int getAbsent();
     
-    void setRecord(int newRecord);
-    void setID (int newID);
-    void setFirst(string newFirst);
-    void setLast(string newLast);
-    void setEmail(string newEmail);
-    void setUnits(string newUnits);
-    void setMajor(string newMajor);
-    void setLevel(string newLevel);
+		void setRecord(int newRecord);
+		void setID (int newID);
+		void setFirst(string newFirst);
+		void setLast(string newLast);
+		void setEmail(string newEmail);
+		void setUnits(string newUnits);
+		void setMajor(string newMajor);
+		void setLevel(string newLevel);
+		void setDate(string newDate, int index);
+		void setAbsent(int newAbsent);
     
-  private:
-    int recordNum;
-    int idNum;
-    string firstName; // not sure if you have to put it this way
-    string lastName; // not sure if you have to put it this way
-    string email;
-    string units; // number of credits for class or AU for audit
-    string major;
-    string gradeLevel; // freshman, sophomore, junior, or senior
+	private:
+		Node *next;
+		int recordNum;
+		int idNum;
+		string firstName; // not sure if you have to put it this way
+		string lastName; // not sure if you have to put it this way
+		string email;
+		string units; // number of credits for class or AU for audit
+		string major;
+		string gradeLevel; // freshman, sophomore, junior, or senior
+		int absent; // number of absences
+		string date[]; // storing dates when the student is absent
 };
 
 #endif
