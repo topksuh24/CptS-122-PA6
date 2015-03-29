e704bc5ba4a754be178e0867fd633156ef889336
topksuh24 <topksuh24@users.noreply.github.com>
2015-03-29 16:15:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,47 +24,51 @@ int main (void)
 	{
 		// display the menu/choices
 		courseList -> menu();
-		cin >> selection;
 
-		switch(selection)
+		do
 		{
-			case 1:
-				// import course list
-				while(file.good())
-				{
-					Node *inputNode = new Node();
-					courseList -> getCourseList(masterList);
-				}
-				cout << "Data has been imported." << endl;
-				break;
+			cin >> selection;
 
-			case 2:
-				// load master list
-				while(masterList != NULL)
-				{
-					courseList -> printMasterList(masterList);
-				}
-				cout << "Master List is loaded." << endl;
-				break;
+			switch(selection)
+			{
+				case 1:
+					// import course list
+					while(file.good())
+					{
+						Node *inputNode = new Node();
+						courseList -> getCourseList(masterList);
+					}
+					cout << "Data has been imported." << endl;
+					break;
 
-			case 3:
-				// store master list
-				courseList -> storeMasterList(masterList);
-				cout << "Master List is stored." << endl;
-				break;
+				case 2:
+					// load master list
+					while(masterList != NULL)
+					{
+						courseList -> printMasterList(masterList);
+					}
+					cout << "Master List is loaded." << endl;
+					break;
 
-			case 4:
-				// mark absences
-				courseList -> markAbsent(masterList);
-				cout << "You have marked absences." << endl;
-				break;
+				case 3:
+					// store master list
+					courseList -> storeMasterList(masterList);
+					cout << "Master List is stored." << endl;
+					break;
 
-			case 5:
-				// generate report
-				courseList -> generateReport(masterList);
-				cout << "Report is generated." << endl;
-				break;
-		}
+				case 4:
+					// mark absences
+					courseList -> markAbsent(masterList);
+					cout << "You have marked absences." << endl;
+					break;
+
+				case 5:
+					// generate report
+					courseList -> generateReport(masterList);
+					cout << "Report is generated." << endl;
+					break;
+			}
+		} while(selection < 1 && selection > 6);
 	} while(selection != 6);
 	
 	return 0;
